e233f42ceb4151e585ec71d1cbe081f6c20d9972
Leonardo Kewitz <leokewitz@gmail.com>
2017-12-07 17:23:25 -0200

    feat: Add Fork screen

--- app/lib/medium.js
+++ app/lib/medium.js
@@ -26,19 +26,33 @@ class Medium {
   }
 
   async loadInfo () {
-    const info = await this.dat.getInfo()
-    const blog = await this.dat.readFile('/blog.json')
+    this.info = await this.dat.getInfo()
+    this.blog = await this.dat.readFile('/blog.json')
       .then(
         config => JSON.parse(config),
         () => { console.error('/blog.json not found') }
       )
-    return { ...info, ...blog }
+    return { ...this.info, ...this.blog }
   }
 
   async loadStyle () {
     return this.dat.readFile('/style.css')
       .then(appendStyle)
       .catch(() => { console.error('/style.css not found') })
+  }
+
+  async fork ({ author, description, photo, title }) {
+    const fork = await window.DatArchive.fork(this.dat.url, { title, description })
+    const display = `/author.${photo.ext}`
+    await fork.unlink(this.blog.display)
+    await fork.rmdir('articles', { recursive: true })
+    await fork.mkdir('articles')
+    await fork.writeFile(display, photo.data, { encoding: 'base64' })
+    await fork.writeFile('/blog.json', JSON.stringify({ author, display }))
+    const date = (new Date()).toISOString().slice(0, 10)
+    await fork.writeFile('articles/hello.md', `date: ${date}\ntitle: First Post\n\nThis is my first post.`)
+    await fork.commit()
+    return fork.url
   }
 }
 
--- app/view.js
+++ app/view.js
@@ -3,10 +3,12 @@ import { h } from 'hyperapp'
 import ArticleList from 'components/ArticleList'
 import Footer from 'components/Footer'
 import Header from 'components/Header'
+import Fork from 'components/Fork/Form.jsx'
 
 export default state => actions =>
   <main>
     <Header {...state} />
+    { (state.page === 'fork') && <Fork {...actions} /> }
     <ArticleList {...state} />
     <Footer />
   </main>
